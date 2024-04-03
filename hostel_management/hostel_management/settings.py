e221f184da143f30448a2f08614260a5f04dfce2
SujanPrasadPandey <88sujanpandey@gmail.com>
2024-04-03 11:38:43 +0545

    django settings apps udpate

--- hostel_management/hostel_management/settings.py
+++ hostel_management/hostel_management/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'booking_management',
+    'feedback_management',
+    'notice_board',
+    'room_management',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'hostel_management.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
